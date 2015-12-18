b2649dd3482371d6401ffdbf15aba2613d05909f
Nick <handnf@gmail.com>
2015-12-18 16:30:46 -0500

    Libraries and flash 1.5. Close #2

---
+++ libraries/Adafruit_BMP280/Adafruit_Sensor.h
@@ -0,0 +1,87 @@
+/*
+* Copyright (C) 2008 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software< /span>
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/* Update by K. Townsend (Adafruit Industries) for lighter typedefs, and
+ * extended sensor support to include color, voltage and current */
+ 
+#ifndef _ADAFRUIT_SENSOR_H
+#define _ADAFRUIT_SENSOR_H
+
+//#if ARDUINO >= 100
+ //#include "Arduino.h"
+ //#include "Print.h"
+//#else
+ //#include "WProgram.h"
+//#endif
+
+/* Intentionally modeled after sensors.h in the Android API:
+ * https://github.com/android/platform_hardware_libhardware/blob/master/include/hardware/sensors.h */
+
+/** Sensor types */
+/*typedef enum
+{
+  SENSOR_TYPE_PRESSURE              = (6),
+  SENSOR_TYPE_RELATIVE_HUMIDITY     = (12),
+  SENSOR_TYPE_AMBIENT_TEMPERATURE   = (13),
+} sensors_type_t;*/
+
+/* Sensor event (36 bytes) */
+/** struct sensor_event_s is used to provide a single sensor event in a common format. */
+//typedef struct
+//{
+  //  int32_t version;                          /**< must be sizeof(struct sensors_event_t) */
+  //  int32_t sensor_id;                        /**< unique sensor identifier */
+  //  int32_t type;                             /**< sensor type */
+  //  int32_t reserved0;                        /**< reserved */
+  //  int32_t timestamp;                        /**< time is in milliseconds */
+  //  union
+  //  {
+  //      float           temperature;          /**< temperature is in degrees centigrade (Celsius) */
+  //      float           pressure;             /**< pressure in hectopascal (hPa) */
+  //      float           relative_humidity;    /**< relative humidity in percent */
+  //  };
+//} sensors_event_t;
+
+/* Sensor details (40 bytes) */
+/** struct sensor_s is used to describe basic information about a specific sensor. */
+//typedef struct
+//{
+  //  char     name[12];                        /**< sensor name */
+  //  int32_t  version;                         /**< version of the hardware + driver */
+  //  int32_t  sensor_id;                       /**< unique sensor identifier */
+  //  int32_t  type;                            /**< this sensor's type (ex. SENSOR_TYPE_LIGHT) */
+  //  float    max_value;                       /**< maximum value of this sensor's value in SI units */
+  //  float    min_value;                       /**< minimum value of this sensor's value in SI units */
+  //  float    resolution;                      /**< smallest difference between two values reported by this sensor */
+  //  int32_t  min_delay;                       /**< min delay in microseconds between events. zero = not a constant rate */
+//} sensor_t;
+
+class Adafruit_Sensor {
+ public:
+  // Constructor(s)
+  Adafruit_Sensor() {}
+  virtual ~Adafruit_Sensor() {}
+
+  // These must be defined by the subclass
+  //virtual void enableAutoRange(bool enabled) {};
+  //virtual bool getEvent(sensors_event_t*) = 0;
+  //virtual void getSensor(sensor_t*) = 0;
+  
+ private:
+  bool _autoRange;
+};
+
+#endif
